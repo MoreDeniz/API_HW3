992d908b475f1ee414909dafdfcd759cc2184a54
maya <you@example.com>
2024-02-24 10:37:01 +0700

    history added

--- index.js
+++ index.js
@@ -1,39 +1,53 @@
+const client_id = "your ACCESS_KEY";
 
-const client_id = 'your ACCESS_KEY';
+const imgContainer = document.querySelector(".img-container");
+const photo = document.querySelector(".img");
+const author = document.querySelector(".author");
+const likesCounter = document.querySelector(".likes-counter");
+const likeBtn = document.querySelector(".like-btn");
+const prevBtn = document.querySelector(".prev-btn");
 
-const likesCounter = document.querySelector|('.likes-counter');
-const likeBtn = document.querySelector|('.like-btn');
-const prevBtn = document.querySelector|('.prev-btn');
-
-const photoEl = document.querySelector('.photo');
-const imgContainer = document.querySelector('.img-container');
-const photo = document.querySelector('.img');
-const author = document.querySelector('.author');
+let history = JSON.parse(localStorage.getItem("history")) || [];
 
 // Получить данные с сервера
 async function getImagesFetch() {
-    try {
-      const response = await fetch(
-        `https://api.unsplash.com/photos/random?client_id=${client_id}`
-      );
-      if (!response.ok) {
-        throw new Error("Сервер встал");
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/photos/random?client_id=${client_id}`
+    );
+    if (!response.ok) {
+      throw new Error("Сервер встал");
+    }
+
+    const requestedData = await response.json();
+    console.log(requestedData);
+
+    const photoURL = requestedData.urls.small;
+    const authorName = requestedData.user.name;
+    const authorUsername = requestedData.user.username;
+
+    photo.src = photoURL;
+    author.textContent = `Photo by ${authorName} (@${authorUsername})`;
+
+    // Добавить фото в историю
+    history.unshift({ photoURL, authorName, authorUsername });
+    localStorage.setItem("history", JSON.stringify(history));
+
+    // Предыдущее фото
+    prevBtn.addEventListener("click", () => {
+      if (history.length > 1) {
+        history.shift();
+        const prevImage = history[0];
+
+        photo.src = prevImage.photoURL;
+        author.textContent = `Photo by ${prevImage.authorName} (@${prevImage.authorUsername})`;
+      } else {
+        alert("No previous images");
       }
-
-      const requestedData = await response.json();
-      console.log(requestedData);
-
-      const photoURL = requestedData.urls.small;
-      const authorName = requestedData.user.name;
-      const authorUsername = requestedData.user.username;
-
-    photo.src  = photoURL;
-    author.textContent = `Author: ${authorName } (@${authorUsername})`;
-    
-
-    } catch (err) {
-      throw err;
-    }  
+    });
+  } catch (err) {
+    throw err;
   }
+}
 
 window.onload = getImagesFetch;
